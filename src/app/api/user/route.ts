9aa0ae35a2163e2b6976a9af075047cc2afd1cdb
panudet ingai <codedroy3@gmail.com>
2024-07-26 09:32:13 +0700

    Refactor user API route to use consistent naming and error handling

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -12,9 +12,54 @@ client.connect();
 
 export async function GET() {
     try {
-        const result = await client.query('SELECT * FROM tbl_student');
+        const result = await client.query('SELECT * FROM tbl_users');
         return NextResponse.json(result.rows);
     } catch (err) {
         return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
+    }
+}
+
+export async function POST() {
+    try {
+        return new Response(JSON.stringify({ message: "POST DATA OK" }), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+        });
+    } catch (error) {
+        console.error(error);
+        return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+        });
+    }
+}
+//-------------------------------------------------------------------------------------
+export async function PUT() {
+    try {
+        return new Response(JSON.stringify({ message: "PUT DATA OK" }), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+        });
+    } catch (error) {
+        console.error(error);
+        return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+        });
+    }
+}
+//-------------------------------------------------------------------------------------
+export async function DELETE() {
+    try {
+        return new Response(JSON.stringify({ message: "DELETE DATA OK" }), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+        });
+    } catch (error) {
+        console.error(error);
+        return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+        });
     }
 }
