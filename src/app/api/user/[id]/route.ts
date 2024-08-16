4672e0c7d88dc78b5635e14fac7f890ffe1404fa
panudet ingai <codedroy3@gmail.com>
2024-08-16 14:22:08 +0700

    refactor: Update user API route to include PUT function for updating user information

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -22,6 +22,24 @@ export async function GET(req: Request, { params }: { params: { id: number } }) 
     }
 }
 
+export async function PUT(req: Request, {params} : {params : {id: number}}) {
+    try {
+        const { firstname, lastname, username, password } = await req.json();
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const res = await client.query(`UPDATE tbl_users SET (firstname, lastname, username, password) = ('${firstname}', '${lastname}', '${username}', '${hashedPassword}') WHERE id = ${params.id} RETURNING *`);
+        if(res.rows.length === 0) {
+            return NextResponse.json({ error: 'User not found' }, { status: 404 });
+        }
+        return NextResponse.json({ message: 'User updated successfully' });
+    } catch (error) {
+        console.error(error);
+        return new Response(JSON.stringify({error}), {
+            status: 500,
+            headers: { "Content-Type": "application/json", 'Access-Control-Allow-Origin': '*' },
+        });
+    }
+}
+
 export async function DELETE(req: Request, { params }: { params: { id: number } }) {
     try {
         const res = await client.query(`DELETE FROM tbl_users WHERE id =  ${params.id}`);
