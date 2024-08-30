a3801751bba4bf12246dd91f5fdfbed948115250
panudet ingai <codedroy3@gmail.com>
2024-08-30 10:16:24 +0700

    refactor: Update Access-Control-Allow-Origin header in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -5,30 +5,12 @@ const nextConfig = {
       {
         // Routes this applies to
         source: "/api/:path*",
-        // Headers
         headers: [
-          // Allow for specific domains to have access or * for all
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // Change to specific origin if needed
-            // value: process.env.ALLOWED_ORIGIN,
-          },
-          // Allows for specific methods accepted
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          // Allows for specific headers accepted (These are a few standard ones)
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-          // Allows credentials to be included in requests
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-        ],
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
       },
     ];
   },
