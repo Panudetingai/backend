b24db047eede16ed5b883b2ec6e838d98574052a
panudet ingai <codedroy3@gmail.com>
2024-08-30 14:04:47 +0700

    refactor: Update Access-Control-Allow-Origin header in next.config
    This commit updates the Access-Control-Allow-Origin header in the next.config file. The previous value of "*" has been changed to "https://panudet-learnapp.vercel.app" to specify a specific origin. This change allows for more granular control over cross-origin resource sharing. Additionally, the commit adds the Access-Control-Allow-Methods and Access-Control-Allow-Headers headers to specify the allowed methods and headers for CORS requests.

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,12 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // Routes this applies to
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://panudet-learnapp.vercel.app" }, // Replace with your actual frontend origin
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" }, // Include OPTIONS for preflight requests
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          { key: "Access-Control-Allow-Origin", value: "https://panudet-learnapp.vercel.app" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization, X-Requested-With" },
         ],
       },
     ];
